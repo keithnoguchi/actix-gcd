8d6da3a2eb09022bcfc6ef34bb5e5fed42e64053
Keith Noguchi <keith@noguchi.us>
2023-03-04 21:45:55 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let app = || App::new().route("/", web::get().to(index));
+
+    HttpServer::new(app)
+        .bind("127.0.0.1:3000")?
+        .run()
+        .await
+}
+
+async fn index(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
